4a897babbc5ff30f5eb858967cd971bf62d6a752
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-01-03 17:29:25 +0530

    add

--- username.sh
+++ username.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-echo "kindly enter your username::" ; read USER
-echo "please enter your password::"; read -s PASS
+echo "kindly enter your username::"  read USER
+echo "please enter your password::" read -s PASS
 echo "Password entered is:: $PASS"
