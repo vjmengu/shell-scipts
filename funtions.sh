4d19f1cfaeb89c2c3fa999a7b61af285077e548b
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-01-04 14:52:43 +0530

    tree&sqlserver

--- funtions.sh
+++ funtions.sh
@@ -10,13 +10,13 @@ then
     dnf install $2 -y
     if [ $1 -ne 0 ]
     then
-        echo "$2...$R failure!"
+        echo -e "$2...$R failure!"
         exit 1
     else
-        echo "$2...$G Sucess!"
+        echo -e "$2...$G Sucess!"
     fi
 else
-    echo "$2 already $Y installed"
+    echo -e "$2 already $Y installed"
 fi
 }
 if [ $USERID -ne 0 ]
