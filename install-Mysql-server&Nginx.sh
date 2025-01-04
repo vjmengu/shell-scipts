6b26cd2599e2e410067f69f9a70e067d2c8efcc1
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-01-04 13:31:07 +0530

    tree&sqlserver

---
+++ install-Mysql-server&Nginx.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+USERID=$(id -u)
+if [ $USERID -ne 0 ]
+then
+    echo "you need sudo access to install a package"
+    exit 1
+fi
+
+dnf list installed mysql-server
+if [ $? -ne 0 ]
+then
+    dnf install mysql-server -y
+    if [ $? -ne 0 ]
+    then
+        echo "installing mysql-serveree...failure!"
+        exit 1
+    else
+        echo "installing mysql-server...Sucess!"
+else
+    echo "mysql-server already installed"
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]
+then
+    dnf install nginx -y
+    if [ $? -ne 0 ]
+    then
+        echo "installing nginx...failure!"
+        exit 1
+    else
+        echo "installing nginx...Sucess!"
+else
+    echo "nginx already installed"
+
+
