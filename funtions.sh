9f4c0aeebfe573f377165e75260ad442b95ad87a
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-01-04 14:48:15 +0530

    tree&sqlserver

--- funtions.sh
+++ funtions.sh
@@ -1,18 +1,22 @@
 #!/bin/bash
 USERID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
 Validate(){
 if [ $1 -ne 0 ]
 then
     dnf install $2 -y
     if [ $1 -ne 0 ]
     then
-        echo "$2...failure!"
+        echo "$2...$R failure!"
         exit 1
     else
-        echo "$2...Sucess!"
+        echo "$2...$G Sucess!"
     fi
 else
-    echo "$2 already installed"
+    echo "$2 already $Y installed"
 fi
 }
 if [ $USERID -ne 0 ]
