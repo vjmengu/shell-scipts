30d8ba83f74a7cd3ae303db0b0db93c6df7df3ae
vijay john mengu <vijayjohnmengu@vijays-MacBook-Air.local>
2025-01-06 20:52:12 +0530

    database-server

---
+++ database-server-shellscript.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+USER=$(id -u)
+
+if [ $USER -ne 0 ]
+then
+    echo "need sudo access to execute this script"
+    exit 1
+else
+    dnf list installed mysql-server
+    if [ $? -ne 0 ]
+    then
+        dnf install mysql-server -y
+        if [ $? -ne 0 ]
+        then
+            echo "mysql-server not installed"
+            exit 1
+        else
+            echo "mysql-server installed"
+        fi
+    else
+        echo "mysql-sever already installed"
+    fi
+fi
+systemctl start mysqld
+systemctl enable mysqld
